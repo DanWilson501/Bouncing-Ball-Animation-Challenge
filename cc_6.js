2d4816a023da5ceb04d31c1e6b92957ddf39f63f
Daniel Wilson <danwilson501@gmail.com>
2024-06-04 21:20:54 -0400

    Corrected Ball class to to include coordinate change properties (dx, dy). Created Draw function for the ball constructor.

--- cc_6.js
+++ cc_6.js
@@ -1,8 +1,19 @@
 class Ball {
-    constructor(x, y, radius, color) {
+    constructor(x, y, radius, dx, dy, color) {
         this.x = x;
         this.y = y;
         this.radius = radius;
+        this.dx = dx;
+        this.dy = dy;
         this.color = color;
     }
+
+    draw(context) {
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        context.fillStyle = this.color;
+        context.fill();
+        context.closePath();
+    }
+
 }
